b74a2e44696af1c688db0d2d936599481fc98357
rayman-zhao <zhao_yu@me.com>
2026-07-26 10:11:15 +0800

    Refactor file name

---
+++ Sources/RsUI/Support/ProgressBar+Extensions.swift
@@ -0,0 +1,25 @@
+import Foundation
+import Observation
+import WinUI
+
+/// FIXME: Must inherit ProgressBar, otherwise can't be triggered the second change of the observation
+public class ProgressBarEx: ProgressBar {
+}
+extension ProgressBarEx {
+    public func startObserving<Element>(
+        _ emit: @escaping @Sendable () -> Element,
+        onChanged: @escaping @MainActor (ProgressBar, Element) -> Void
+    ) {
+        let obs = Observations(emit)
+
+        Task { [weak self] in
+            for await value in obs {
+                guard let self else { return }
+                await MainActor.run { [weak self] in
+                    guard let self else { return }
+                    onChanged(self, value)
+                }
+            }
+        }
+    }
+}
